dada9a4e2b9341b478ed9bad416c81480c37dc6c
Pram Attale <pram@attale.com>
2017-12-08 18:59:21 +0000

    adding spring-boot starter

---
+++ src/main/java/com/naughtyzombie/aws/awslambdaspringboot/AwsLambdaSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.naughtyzombie.aws.awslambdaspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsLambdaSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsLambdaSpringBootApplication.class, args);
+	}
+}
